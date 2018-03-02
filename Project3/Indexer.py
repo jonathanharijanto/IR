82f9b0c7b2dd131adebe99389d7a5da267bf70cf
Avinash Kumar <avinash0161@yahoo.com>
2018-03-01 22:52:28 -0800

    Added a few lines to the output file

--- Project3/Indexer.py
+++ Project3/Indexer.py
@@ -7,6 +7,8 @@ path = "webpages_clean/"
 
 os.getcwd()
 
+global totalDocuments
+
 def isValid(word):
     if word.isdigit():
         return False
@@ -17,12 +19,15 @@ def main():
     inverted_index = defaultdict(lambda: defaultdict(lambda: list()))
     word_count = {}
     w = open("monitoroutput.txt", 'w')
+    global totalDocuments
+    totalDocuments = 0
     for parentfilename in os.listdir(path):
         if os.path.isdir(path + parentfilename):
             print "parentdir: " + path + parentfilename
             w.write("PARENT DIRECTORY: " + path + parentfilename + "\n")
             for filename in os.listdir(path+parentfilename):
                 if os.path.isfile(path + parentfilename + "/" + filename):
+                    totalDocuments += 1
                     print "\tSUB DIRECTORY: " + path + parentfilename + "/" + filename
                     w.write("\tSUB DIRECTORY: " + path + parentfilename + "/" + filename + "\n")
                     docID = parentfilename + "_" + filename
@@ -50,6 +55,8 @@ def main():
 
 def outputBeautify(a_dictionary):
     w = open('outputfile.txt', 'w')
+    w.write("Total unqiue words = " + str(len(a_dictionary.keys())) + "\n")
+    w.write("Total documents = " + str(totalDocuments) + "\n")
     for i in a_dictionary:
         w.write(str(i) + " --> " )
         for j in a_dictionary[i]:
