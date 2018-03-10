558060f41176e49685f2fa730ca7835bf203c85b
jonathanharijanto <harijant@uci.edu>
2018-03-10 07:26:13 -0800

    kyungwoo snippet

--- Project3/Indexer.py
+++ Project3/Indexer.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import os
 import re
 from collections import defaultdict
+from collections import OrderedDict
 import json
 
 path = "webpages_clean/"
@@ -57,24 +58,50 @@ def main():
             input = raw_input("Search here: ")
             query_index(input, json_data, json_url_data)
 
+def showSnippet(docID, positions):
+    file = open((path + docID), "r")
+    cleantxtfile = BeautifulSoup(file, "lxml").text
+    line = re.sub(r'\W', ' ', cleantxtfile).lower().split()
+    eof = len(line)
+    for p in positions:
+        start = p-5 if p>=5 else 0
+        end = p+5 if (p+5)<=eof else eof
+        for i in range(start,end):
+            print line[i].encode('ascii', 'ignore'),
+        else:
+            print
+    return
+
 # Scalable query index, could handle n-numbers of query
 # Need to improve: the order of the query
 def query_index(input, index_dict, url_dict):
     query = re.sub(r'\W', ' ', input).lower().split()
-    listOfList = []
+    resultList = []
     for q in query:
-        temp = []
         if q in index_dict:
-            for docID in index_dict[q]:
-                temp.append(docID.encode('ascii', 'ignore'))
-        listOfList.append(temp)
-    intersection = list(set(listOfList[0]).intersection(*listOfList))
-    print intersection
-    for docID in intersection:
-        docID = docID.replace('_', '/')
+            resultList.append(index_dict[q])
+
+    intersectKeys = set(resultList[0].keys())
+    for r in resultList[1:]:
+        intersectKeys &= set(r.keys())
+
+    intersectDic = OrderedDict()
+    for key in intersectKeys:
+        for r in resultList:
+            intersectDic[key] = r[key]
+
+    i = 0
+    for key, positions in intersectDic.iteritems():
+        if i == 5:
+            break
+
+        docID = key.replace('_', '/')
         if docID in url_dict:
             url = url_dict[docID]
-            print "DocID: " + str(docID) + ", URL: " + url
+            #print "DocID: " + str(docID) + ", URL: " + url
+            print("http://"+url)
+            showSnippet(docID, positions)
+            i += 1
 
 def outputNormal(a_dictionary):
     with open('output.json', 'w') as fp:
@@ -92,7 +119,7 @@ def outputBeautify(a_dictionary, totalDocuments):
     w.write("Total size of index on disk: " + str(os.path.getsize('outputbeauty.txt')) + " bytes")
     w.close()
 
-# Added by Qiushi, for generate a json file of our index
+# Added bushi, for generate a json file of our index
 def outputJSON(a_dictionary):
     w = open('index.json', 'w')
     w.write("{\n")
